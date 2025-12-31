33686afdde691f59515d65687a372f048b8a16cf
Meghsham Kapure <meghshamvinayakkapure@gmail.com>
2025-12-31 17:05:00 +0530

    implement ecommerce domain models for users, products, categories and orders

---
+++ src/models/ecommerce/order.model.js
@@ -0,0 +1,58 @@
+import mongoose from "mongoose";
+
+const orderItemSchema = new mongoose.Schema({
+  productId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Product",
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+});
+
+const orderSchema = mongoose.Schema(
+  {
+    orderPrice: {
+      type: Number,
+      required: true,
+    },
+    customer: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    orderItems: { type: [orderItemSchema] },
+
+    // does the same but with different approch
+    // orderItems: {
+    // type: [
+    //     {
+    //     productId: {
+    //         type: mongoose.Schema.Types.ObjectId,
+    //         ref: "Product",
+    //     },
+    //     quantity: {
+    //         type: Number,
+    //         required: true,
+    //     },
+    //     },
+    // ],
+    // },
+
+    adrress: {
+      type: String,
+      required: true,
+    },
+
+    status: {
+      type: String,
+      enum: ["PENDING", "CANCELLED", "DELIVERED"],
+      default: "PENDING",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Order = mongoose.model("order", orderSchema);
